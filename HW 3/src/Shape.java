95b140697ef1957d4de6dcff69086a58e762d851
kchoi143 <49960277+kchoi143@users.noreply.github.com>
2019-05-09 14:23:19 -0700

    Add files via upload

---
+++ HW 3/src/Shape.java
@@ -0,0 +1,11 @@
+/**
+ * This interface should describe the minimal 
+ * information about a viewer needed for the DrawingModel to interact with it (probably just an update method).
+ *
+ */
+
+import java.awt.Graphics;
+
+public interface Shape {
+	void draw(Graphics g);
+}
