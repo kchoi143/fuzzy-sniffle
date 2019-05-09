0ac9a7d85234398ecfefdabf9343139ef881c98d
kchoi143 <49960277+kchoi143@users.noreply.github.com>
2019-05-09 14:21:30 -0700

    Delete AbstractShape.java

--- src/AbstractShape.java
+++
@@ -1,6 +0,0 @@
-
-public abstract class AbstractShape implements Shape {
-	// Add instance variables, additional methods and default 
-	// implementations here once it becomes clear that all Shapes need to have such fields and methods.
-	
-}
